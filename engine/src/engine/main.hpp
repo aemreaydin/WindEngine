8075e73e4fdbecdf603504dcb0313ce10b4ee56a
Emre AYDIN <aeaydin1@gmail.com>
2023-11-27 15:32:07 -0500

    Removed macOS version warning

--- engine/src/engine/main.hpp
+++ engine/src/engine/main.hpp
@@ -8,28 +8,13 @@
 
 auto CreateApp() -> std::unique_ptr<WindEngine::App>;
 
-#pragma pack( push, 1 )
-
-struct TestStruct
-{
-    U32 val;
-    U16 val2;
-    U8 val3;
-};
-
-#pragma pack( pop )
-
 auto main() -> int
 {
     WindEngine::Core::Logger::Initialize();
 
     WindEngine::Core::Memory::LinearAllocator allocator { 256 };
     allocator.Init();
-
-    *( static_cast<U8*>( allocator.Allocate( sizeof( U8 ), true ) ) ) = 1;
-    *( static_cast<TestStruct*>( allocator.Allocate( sizeof( TestStruct ), true ) ) ) = { 4, 5, 3 };
-    *( static_cast<TestStruct*>( allocator.Allocate( sizeof( TestStruct ), true ) ) ) = { 6, 7, 2 };
-
+    
     {
         WindEngine::Engine engine { CreateApp() };
         engine.Run();
